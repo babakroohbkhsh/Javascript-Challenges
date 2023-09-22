645b8f0ac23a9e988ea3a953210f93b4a16a4770
Babak Roohbakhsh <roohbakhshbabak@hotmail.com>
2023-09-22 00:50:40 -0700

    More Array challeges

--- Array/scriptArray.js
+++ Array/scriptArray.js
@@ -46,6 +46,7 @@ log(cleanUpArray(["7", "4", "8"]));
 // and calculate the average of the remaining numbers.
 
 function trimmedAverages(arr) {
+  // sort min to max
   let sorted = arr.sort((a, b) => a - b);
   sorted.pop();
   sorted.shift();
@@ -53,3 +54,32 @@ function trimmedAverages(arr) {
 }
 
 log(trimmedAverages([4, 5, 7, 100]));
+
+// In mathematics, an Arithmetic Progression (AP) is a sequence of numbers such that the difference between
+// the consecutive terms is constant. Create a function that takes three arguments:
+
+//     First element of the sequence first
+//     Constant difference between the elements diff
+//     Total numbers in the sequence n
+
+// Return the first n elements of the sequence with the given common difference diff.
+// The final result should be a string of numbers, separated by comma and space.
+
+function arithmeticProgression(start, diff, n) {
+  const counter = [];
+  counter[0] = start;
+  for (let i = 1; i < n; i++) {
+    counter.push((start += diff));
+  }
+  return counter.join(", ");
+}
+
+// log(arithmeticProgression(1, 2, 5));
+// log(arithmeticProgression(1, 0, 5));
+
+// Given an array of numbers, return true if the sum of the array is less than 100; otherwise return false.
+
+function arrayLessThan100(arr) {
+  return arr.map((x) => x + x) < 100 ? true : false;
+}
+log(arrayLessThan100([77, 30]));
